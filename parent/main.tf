c1baf59dac07cc7ee4b9f64b139ba004e511d9a7
rajat-sharma8181 <rajats12120@gmail.com>
2025-08-29 15:50:21 +0530

    storage removed

--- parent/main.tf
+++ parent/main.tf
@@ -4,12 +4,12 @@ module "rg_mod" {
     rg_location = "central india"
 }
 
-module "stg_mod"{
-    depends_on = [ module.rg_mod ]
-    source = "../module/storage"
-    stg_name = "rajat_stg"
-    stg_location = "central india"
-    resource_group_name = "rajat_rg"
-    account_tier = "Standard"
-    account_replication_type = "LRS"
-}
+# module "stg_mod"{
+#     depends_on = [ module.rg_mod ]
+#     source = "../module/storage"
+#     stg_name = "rajat_stg"
+#     stg_location = "central india"
+#     resource_group_name = "rajat_rg"
+#     account_tier = "Standard"
+#     account_replication_type = "LRS"
+# }
