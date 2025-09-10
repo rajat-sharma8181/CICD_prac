140f693755a6c223d2d555c67b665027f763ecfc
rajat-sharma8181 <rajats12120@gmail.com>
2025-09-10 12:32:07 +0530

    new rg added

--- modules/storage/variable.tf
+++ modules/storage/variable.tf
@@ -1,5 +1,5 @@
-variable "stg_name" {}
-variable "stg_location" {}
-variable "resource_group_name" {}
-variable "account_tier" {}
-variable "account_replication_type" {}
+# variable "stg_name" {}
+# variable "stg_location" {}
+# variable "resource_group_name" {}
+# variable "account_tier" {}
+# variable "account_replication_type" {}
